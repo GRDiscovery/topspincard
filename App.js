a71e10fc27d74035262595f7bb4f0bbc80dc3ec6
GRDiscovery <ti@grdiscovery.com.br>
2020-06-12 17:05:10 -0300

    tela extrato

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { StyleSheet, Platform, Image, Text, View } from 'react-native'
 import Login from './src/pages/login'
+import Extrato from './src/pages/extrato'
+
 
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
@@ -8,9 +10,12 @@ const AppNavigator = createStackNavigator({
   Login: {
     screen: Login,
   },
+  Extrato: {
+    screen: Extrato,
+  },
 },
 {
-  initialRouteName: 'Login',
+  initialRouteName: 'Extrato',
   defaultNavigationOptions: {
     headerStyle: {
       backgroundColor: '#dcdcdc',
